a468c447bb684c89933579d7facb04136e3a7f7d
Laminatqw <victor.kinash47@gmail.com>
2024-07-29 18:37:15 +0300

    5th lesson(form and validation)

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component, FC} from 'react';
 import './App.css';
 import UserComponent from './components/UserComponent';
 import UserPostComponent from './components/UserPostComponent';
 import { getUsers, getUserPosts } from './services/user.api.services';
 import IUser from './Model/IUser';
 import { IPost } from './Model/IPost';
+import FormComp from "./components/FormComp";
 
-interface AppState {
-    users: IUser[];
-    selectedUserId: number | null;
-    posts: IPost[];
-}
 
-class App extends Component<{}, AppState> {
-    state: AppState = {
-        users: [],
-        selectedUserId: null,
-        posts: [],
-    };
-
-    componentDidMount() {
-        getUsers()
-            .then(response => {
-                this.setState({ users: response.data.users });
-            })
-            .catch(error => {
-                console.error('Error fetching users:', error);
-            });
-    }
-
-    handleUserClick = (id: number) => {
-        getUserPosts(id)
-            .then(response => {
-                this.setState({ posts: response.data.posts });
-            })
-            .catch(error => {
-                console.error('Error fetching user posts:', error);
-            });
-    };
-
-    render() {
-        const { users, posts } = this.state;
-        return (
-            <div>
-                {users.map(user => (
-                    <UserComponent key={user.id} user={user} onUserClick={this.handleUserClick} />
-                ))}
-                <h2>Posts:</h2>
-                <UserPostComponent posts={posts} />
-            </div>
-        );
-    }
-}
+const App: React.FC = () => {
+    return (
+        <div>
+            <FormComp/>
+        </div>
+    );
+};
 
 export default App;
